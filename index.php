02be1ca0416024ea5922c96978d5d22da97471bd
David Carter <dajocarter@gmail.com>
2015-09-15 01:08:15 -0400

    Upgrade homepage

--- index.php
+++ index.php
@@ -6,67 +6,54 @@ $pageTitle = "My Brain on Anatomy";
 $section = "none";
 include(ROOT_PATH . "templates/header.php"); ?>
 
-<div class="row">
-  <h1>Check out my Histology Cases!</h1>
-  <ul class="list">
-    <li>
-      <a href="/Learning-Objectives">Learning Objectives</a>
-    </li>
-    <!--li>
-      <a href="/Pre-Test">Pre-Test</a>
-    </li-->
-    <li>Cases
-      <ul>
-        <li>
-          <a href="/A-Mysterious-Mass/pg/1.php">A Mysterious Mass</a>
-          <br><span class="italic">A 33-year-old male presents with an anterior mediastinal mass…</span>
-          <ul>
-            <li>
-              <a href="/A-Mysterious-Mass/references.php">References</a>
-            </li>
-          </ul>
-        </li>
-        <li>
-          <a href="/A-Night-in-the-ER/pg/1.php">A Night in the E.R.</a>
-          <br><span class="italic">While working in the emergency room one evening, an unconscious 58-year-old male is rushed into the scene…</span>
-          <ul>
-            <li>
-              <a href="/A-Night-in-the-ER/references.php">References</a>
-            </li>
-          </ul>
-        </li>
-        <li>
-          <a href="/The-Suspicious-Lesion/pg/1.php">The Suspicious Lesion</a>
-          <br><span class="italic">A 57-year-old female is evaluated for a mammogram with a suspicious lesion…</span>
-          <ul>
-            <li>
-              <a href="/The-Suspicious-Lesion/references.php">References</a>
-            </li>
-          </ul>
-        </li> 
-      </ul>
-    </li>
-    <li>What is a flipped class?
-      <ul>
-        <li>
-          <a target="_blank" href="//elearninginfographics.com/what-is-a-flipped-classroom-infographic-plus-the-educator-guide-to-flipped-classroom/">What is a Flipped Classroom Infographic</a>
-        </li>
-        <li>
-          <a target="_blank" href="//www.youtube.com/watch?v=iQWvc6qhTds">Flipping the Classroom: Explained</a>
-        </li>
-      </ul>
-    </li>
-    <li>Download the HAPS 2015 Workshop Presentation
-      <ul>
-        <li>
-          <a href="files/pptx.php">Download as .pptx</a>
-        </li>
-      </ul>
-    </li>
-  </ul>
 <div class="hero-unit">
   <h1>A560 Histology Cases</h1>
 </div>
+
+<div class="container">
+<div class="row cases margin-top-lg">
+  <h2>Cases</h2>
+  <div class="col-md-4 margin-top-lg">
+    <a class="round-img" href="/A-Mysterious-Mass/pg/1.php"><img src="assets/img/mm-pg1.png"></a>
+    <h3><a href="/A-Mysterious-Mass/pg/1.php">A Mysterious Mass</a></h3>
+    <p class="italic">A 33-year-old male presents with an anterior mediastinal mass…</p>
+    <a href="/A-Mysterious-Mass/references.php">References</a>
+  </div>
+  <div class="col-md-4 margin-top-lg">
+    <a class="round-img" href="/The-Suspicious-Lesion/pg/1.php"><img src="assets/img/tsl-pg1.png"></a>
+    <h3><a href="/The-Suspicious-Lesion/pg/1.php">The Suspicious Lesion</a></h3>
+    <p class="italic">A 57-year-old female is evaluated for a mammogram with a suspicious lesion…</p>
+    <a href="/The-Suspicious-Lesion/references.php">References</a>
+  </div>
+  <div class="col-md-4 margin-top-lg">
+    <a class="round-img" href="/A-Night-in-the-ER/pg/1.php"><img src="assets/img/UL.png"></a>
+    <h3><a href="/A-Night-in-the-ER/pg/1.php">A Night in the E.R.</a></h3>
+    <p class="italic">While working in the emergency room one evening, an unconscious 58-year-old male is rushed into the scene…</p>
+    <a href="/A-Night-in-the-ER/references.php">References</a>
+  </div>
+</div>
+
+<div class="row extras margin-top-lg">
+  <h2>Learn More about the Cases</h2>
+  <div class="col-md-4 margin-top-lg">
+    <a class="round-img" href="/Learning-Objectives"><img src="assets/img/mm-pg1.png"><i class="icon ion-ios-checkmark-empty"></i></a>
+    <h3><a href="/Learning-Objectives">Learning Objectives</a></h3>
+    <p>View the learning objectives for these cases and see which cases the objectives can be found.</p>
+  </div>
+  <div class="col-md-4 margin-top-lg">
+    <a class="round-img" href="javascript:void(0)"><img src="assets/img/tsl-pg1.png"><i class="icon ion-ios-help-empty"></i></a>
+    <h3>What is a flipped class?</h3>
+    <p>Watch this <a class="video-link" href="//www.youtube.com/watch?v=iQWvc6qhTds">video</a> explaining what a flipped 
+    class is, and then check out this 
+    <a target="_blank" href="//elearninginfographics.com/what-is-a-flipped-classroom-infographic-plus-the-educator-guide-to-flipped-classroom/">infographic</a>
+     to learn more about flipped classes.</p>
+  </div>
+  <div class="col-md-4 margin-top-lg">
+    <a class="round-img" href="javascript:void(0)"><img src="assets/img/UL.png"><i class="icon ion-ios-cloud-download-outline"></i></a>
+    <h3>Download Links</h3>
+    <p>Download the HAPS 2015 Workshop Presentation as a <a href="files/pptx.php">.pptx file</a>, 
+    a <a href="files/ppt.php">.ppt file</a>, or a <a href="files/pdf.php">.pdf file</a>.</p>
+  </div>
 
 <?php include(ROOT_PATH . "templates/footer.php"); ?>
 
