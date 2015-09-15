649f67084dfbfd93ffe0440672a50747eb4edf9e
David Carter <dajocarter@gmail.com>
2015-09-15 00:30:47 -0400

    Add period to end of sentences

--- Learning-Objectives/index.php
+++ Learning-Objectives/index.php
@@ -10,26 +10,26 @@ include(ROOT_PATH . "templates/header.php"); ?>
 
 <div class="container cases">
   <ol class="list">
-    <li>Understand that lymphoid organs usually show a network of reticular fibers or epithelial tissue filled with lymphocytes and other cells of the immune system
+    <li>Understand that lymphoid organs usually show a network of reticular fibers or epithelial tissue filled with lymphocytes and other cells of the immune system.
       <ul>
         <li>A Mysterious Mass</li>
         <li>The Suspicious Lesion</li>
       </ul>
     </li>
 
-    <li>Distinguish and recognize the specific structural features and regions of the thymus and understand the functional significance of these features in each
+    <li>Distinguish and recognize the specific structural features and regions of the thymus and understand the functional significance of these features in each.
       <ul>
         <li>A Mysterious Mass</li>
       </ul>
     </li>
 
-    <li>Understand special features of the circulation through the thymus
+    <li>Understand special features of the circulation through the thymus.
       <ul>
         <li>A Mysterious Mass</li>
       </ul>
     </li>
 
-    <li>Understand the major regions of a lymph node and their functions
+    <li>Understand the major regions of a lymph node and their functions.
       <ul>
         <li>The Suspicious Lesion</li>
       </ul>
@@ -41,7 +41,7 @@ include(ROOT_PATH . "templates/header.php"); ?>
       </ul>
     </li>
 
-    <li>Recognize the major unencapsulated collection of lymphocytes in the mucosa-associated lymphoid tissue (MALT), including tonsils, Peyer’s patches, and appendix
+    <li>Recognize the major unencapsulated collection of lymphocytes in the mucosa-associated lymphoid tissue (MALT), including tonsils, Peyer’s patches, and appendix.
       <ul>
         <li>A Night in the ER</li>
         <li>The Suspicious Lesion</li>
@@ -49,19 +49,19 @@ include(ROOT_PATH . "templates/header.php"); ?>
       </ul>
     </li>
 
-    <li>Understand the functions of MALT
+    <li>Understand the functions of MALT.
       <ul>
         <li>A Night in the ER</li>
       </ul>
     </li>
 
-    <li>Know the histological differences between white and red pulp in the spleen and their principal microscopic features
+    <li>Know the histological differences between white and red pulp in the spleen and their principal microscopic features.
       <ul>
         <li>A Night in the ER</li>
       </ul>
     </li>
 
-    <li>Understand the various functions of the spleen and which of its parts are responsible for these functions
+    <li>Understand the various functions of the spleen and which of its parts are responsible for these functions.
       <ul>
         <li>A Night in the ER</li>
       </ul>
