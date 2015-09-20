c4477477c31eb7b61ac847fe172b157bfe39c8e5
David Carter <dajocarter@gmail.com>
2015-09-20 14:44:02 -0400

    Change age of NER patient

--- A-Night-in-the-ER/pg/1.php
+++ A-Night-in-the-ER/pg/1.php
@@ -8,7 +8,7 @@ include(ROOT_PATH . "/A-Night-in-the-ER/index.php");
 
 ?>
 
-<p>While working in the emergency room one evening, an unconscious 58-year-old male is rushed into the scene. You are informed he was in a car accident and has a laceration along the lateral margin of the upper left abdominal quadrant between the 10th and 11th ribs resulting in profuse bleeding.</p>
+<p>While working in the emergency room one evening, an unconscious 41-year-old male is rushed into the scene. You are informed he was in a car accident and has a laceration along the lateral margin of the upper left abdominal quadrant between the 10th and 11th ribs resulting in profuse bleeding.</p>
 
 <p class="bold">Click on the histology slide below that depicts the organ injured.</p>
 
