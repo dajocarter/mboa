b2632e27c5218b9fd90e10b110e27d70715e0259
David Carter <dajocarter@gmail.com>
2015-09-20 23:11:21 -0400

    Update 404

--- 404.php
+++ 404.php
@@ -6,7 +6,8 @@ $pageTitle = "404 Not Found";
 $section = "none";
 include(ROOT_PATH . "templates/header.php"); ?>
 
-<div class="row">
+<div class="container">
+<div>
   <h1>404</h1>
   <p>This is not the page you are looking for.</p>
 
