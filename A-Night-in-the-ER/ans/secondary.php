d2b1509dd6049a604fbe4ce455479125c63e3e87
David Carter <dajocarter@gmail.com>
2016-02-24 20:27:24 -0500

    Remove templates that just redirect

--- A-Night-in-the-ER/ans/secondary.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require_once("../../../templates/config.php");
-
-header("Location: " . BASE_URL . "A560/A-Night-in-the-ER/pg/6");
-
-?>
--- A-Night-in-the-ER/ans/ur.php
+++
@@ -1,4 +0,0 @@
-<?php
-require_once("../../../templates/config.php");
-header("Location: " . BASE_URL . "A560/A-Night-in-the-ER/pg/2");
-?>
