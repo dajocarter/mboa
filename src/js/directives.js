7c2a887ceec8c89901bd5a94804843a25e5f5584
David Carter <dajocarter@gmail.com>
2016-02-26 20:48:54 -0500

    Add options for iframe popups to mfp directive

--- src/js/directives.js
+++ src/js/directives.js
@@ -3,26 +3,37 @@ mboa.directive('mfp', function() {
     restrict: 'A',
     link: function(scope, element, attrs) {
       var defaults = {
-          midClick: true
+          midClick: true,
+          closeBtnInside: false
         },
         options;
-      if (scope.$eval(attrs.mfp)["type"] == 'image') {
-        var imageDefaults = {
-          closeOnContentClick: true,
-          closeBtnInside: false,
-          fixedContentPos: true,
-          mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
-          image: {
-            verticalFit: true
-          },
-          zoom: {
-            enabled: true,
-            duration: 300 // don't forget to change the duration also in CSS
-          }
-        };
-        options = angular.merge({}, imageDefaults, defaults, scope.$eval(attrs.mfp));
-      } else {
-        options = angular.extend({}, defaults, scope.$eval(attrs.mfp));
+      switch (scope.$eval(attrs.mfp)["type"]) {
+        case 'image':
+          var imageDefaults = {
+            closeOnContentClick: true,
+            fixedContentPos: true,
+            mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
+            image: {
+              verticalFit: true
+            },
+            zoom: {
+              enabled: true,
+              duration: 300 // don't forget to change the duration also in CSS
+            }
+          };
+          options = angular.merge({}, imageDefaults, defaults, scope.$eval(attrs.mfp));
+          break;
+        case 'iframe':
+          var iframeDefaults = {
+            preloader: false,
+            fixedContentPos: false,
+            removalDelay: 160,
+            mainClass: 'mfp-fade'
+          };
+          options = angular.merge({}, iframeDefaults, defaults, scope.$eval(attrs.mfp));
+          break;
+        default:
+          options = angular.extend({}, defaults, scope.$eval(attrs.mfp));
       }
 
       $(element).magnificPopup(options);
