207217dbd96bc60c03527f830eca6cad16572818
David Carter <dajocarter@gmail.com>
2016-02-26 20:20:46 -0500

    Add options for image popups to mfp directive

--- src/js/directives.js
+++ src/js/directives.js
@@ -5,7 +5,25 @@ mboa.directive('mfp', function() {
       var defaults = {
           midClick: true
         },
+        options;
+      if (scope.$eval(attrs.mfp)["type"] == 'image') {
+        var imageDefaults = {
+          closeOnContentClick: true,
+          closeBtnInside: false,
+          fixedContentPos: true,
+          mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
+          image: {
+            verticalFit: true
+          },
+          zoom: {
+            enabled: true,
+            duration: 300 // don't forget to change the duration also in CSS
+          }
+        };
+        options = angular.merge({}, imageDefaults, defaults, scope.$eval(attrs.mfp));
+      } else {
         options = angular.extend({}, defaults, scope.$eval(attrs.mfp));
+      }
 
       $(element).magnificPopup(options);
     }
