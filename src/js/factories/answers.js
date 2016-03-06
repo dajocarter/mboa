1f53054dcd9949cc32b94c0085e6f7c5ed95a6bc
David Carter <dajocarter@gmail.com>
2016-03-06 12:40:45 -0500

    Add factories for hints and answers

---
+++ src/js/factories/answers.js
@@ -0,0 +1,43 @@
+angular.module('mboa').factory('Answers', function() {
+  return [
+    ["Simple cuboidal epithelium", "Simple cuboidal"], // 0
+    "Subcapsular sinus", ["Lymph follicle", "Lymph nodule", "Lymphoid follicle", "Lymphoid nodule"],
+    "Cortex",
+    "Paracortex",
+    "Medulla", // 5
+    ["RANKL", "RANK Ligand", "RANK ligand", "Rank ligand"],
+    "Medulla",
+    "Capsule",
+    "Cortex",
+    "Septa", // 10
+    ["Tonsil", "Tonsils"],
+    ["Appendix", "Vermiform appendix"],
+    ["Peyer's patches", "Peyers patches"],
+    "Bone marrow",
+    "Cytokines", // 15
+    "Basophilic",
+    "Form boundaries",
+    "APCs",
+    "Cytoreticulum",
+    "Cytokine secretion", // 20
+    ["High endothelial venules", "High endothelial venule", "HEVs", "HEV"],
+    "Stratified squamous epithelium",
+    "Stave cells",
+    "Thymus",
+    "Germinal center", // 25
+    "Marginal zone",
+    "Central arteriole",
+    "Germinal center", ["Lymph follicle", "Lymph nodule", "Lymphoid follicle", "Lymphoid nodule"], // Repeat of #2
+    "Stratified squamous epithelium", // 30
+    ["Crypt", "tonsillar crypt"],
+    "White pulp",
+    "Red pulp",
+    "Trabecula",
+    "Eosin", // 35
+    ["Silver stain", "periodic acid-Schiff reaction", "PAS reaction"],
+    "sinuses",
+    "cords", // 38
+    ["High endothelial venules", "HEVs"], // Repeat of #21
+    ["afferent lymphatic", "afferent lymphatics", "afferent lymphatic vessels", "afferent lymphatic vessel", "afferent vessels", "afferent vessel"] // 40
+  ];
+});
