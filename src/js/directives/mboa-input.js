514924fbd31bd8944d7fbced6c8010aa03c20116
David Carter <dajocarter@gmail.com>
2016-03-07 01:52:31 -0500

    Bind ngModel for multiple instances in same template

--- src/js/directives/mboa-input.js
+++ src/js/directives/mboa-input.js
@@ -2,8 +2,8 @@ angular.module('mboa').directive('mboaInput', function(Answers, Hints) {
   function getTemplate(tElt, tAttrs) {
     var alertTA = "<textarea class='check-input'></textarea><i class='icon ion-alert-circled input'></i>";
     var alertInput = "<input type='text' class='check-input' /><i class='icon ion-alert-circled input'></i>";
-    var gradedInput = "<input type='text' ng-model='userAnswer' ng-change='gradeAnswer(userAnswer)' class='check-input' /><i ng-show='correct' class='icon ion-ios-checkmark input'></i>";
-    var hintInput = "<input type='text' ng-model='userAnswer' ng-change='gradeAnswer(userAnswer)' class='check-input'><i ng-hide='showHint || correct' class='icon ion-ios-help-empty input' ng-click='toggleHint()'></i><i ng-show='correct' class='icon ion-ios-checkmark input'></i><div ng-show='showHint' class='hint'>{{hint}}<i class='icon ion-ios-close' ng-click='toggleHint()'></i></div>"
+    var gradedInput = "<input type='text' ng-model='bindedModel' ng-change='gradeAnswer(bindedModel)' class='check-input' /><i ng-show='correct' class='icon ion-ios-checkmark input'></i>";
+    var hintInput = "<input type='text' ng-model='bindedModel' ng-change='gradeAnswer(bindedModel)' class='check-input'><i ng-hide='showHint || correct' class='icon ion-ios-help-empty input' ng-click='toggleHint()'></i><i ng-show='correct' class='icon ion-ios-checkmark input'></i><div ng-show='showHint' class='hint'>{{hint}}<i class='icon ion-ios-close' ng-click='toggleHint()'></i></div>"
 
     if (tAttrs.type == "textarea") {
       return alertTA;
@@ -27,7 +27,8 @@ angular.module('mboa').directive('mboaInput', function(Answers, Hints) {
     require: 'ngModel',
     scope: {
       answer: '&',
-      hint: '&'
+      hint: '&',
+      bindedModel: '=ngModel'
     },
     template: getTemplate,
     link: function(scope, element, attrs, ngModel) {
