fce6f8992f2a679f4133595dc645e52146def82d
David Carter <dajocarter@gmail.com>
2016-03-15 02:13:18 -0400

    Get animations closer to what I want

--- src/js/directives/mboa-graphBar.js
+++ src/js/directives/mboa-graphBar.js
@@ -3,7 +3,7 @@ angular.module('mboa').directive('mboaGraphBar', function() {
 
   return {
     restrict: 'E',
-    template: '<div class="graph-bar clearfix"><div class="graph-bar-outline"><div class="graph-bar-color"></div></div><span class="graph-bar-percent"></span></div>',
+    template: '<div class="graph-bar clearfix"><div ng-show="show" class="graph-bar-outline"><div ng-class="{green: right, red: !right}" class="graph-bar-color"></div></div><span ng-show="show" class="graph-bar-percent"></span></div>',
     scope: {
       correct: '@',
       problem: '@',
@@ -11,11 +11,10 @@ angular.module('mboa').directive('mboaGraphBar', function() {
       submitted: '@'
     },
     link: function(scope, element, attrs) {
-      var color = scope.$eval(attrs.correct) ? 'green' : 'red'
-      angular.element(element[0].querySelector('.graph-bar-color')).addClass(color);
-
       attrs.$observe('submitted', function(value) {
         if (scope.$eval(value)) {
+          scope.show = true;
+
           var keys = ['one', 'two', 'three', 'four', 'five'];
           var problem = scope.$eval(attrs.problem);
           var option = scope.$eval(attrs.option)['choice'];
@@ -25,13 +24,16 @@ angular.module('mboa').directive('mboaGraphBar', function() {
             var denom = snap.total;
             var numer = snap[option];
             var percent = numer ? ((numer / denom) * 100) : 0;
-            var roundedPercent = Number(Math.round(percent + 'e3')+'e-3').toFixed(3);
+            var roundedPercent = Number(Math.round(percent + 'e3') + 'e-3').toFixed(3);
             angular.element(element).find('span').text(roundedPercent + "%");
             var colorWidth = (percent * 2) + 'px';
             angular.element(element[0].querySelector('.graph-bar-color')).css({
               width: colorWidth
             });
           });
+
+          scope.right = scope.$eval(attrs.correct) ? true : false;
+          /*angular.element(element[0].querySelector('.graph-bar-color')).addClass(color);*/
         }
       })
     }
--- src/js/directives/mboa-input.js
+++ src/js/directives/mboa-input.js
@@ -3,7 +3,7 @@ angular.module('mboa').directive('mboaInput', function(Answers, Hints) {
     var alertTA = "<textarea class='check-input'></textarea><i class='icon ion-alert-circled input'></i>";
     var alertInput = "<input type='text' class='check-input' /><i class='icon ion-alert-circled input'></i>";
     var gradedInput = "<input type='text' ng-model='bindedModel' ng-change='gradeAnswer(bindedModel)' class='check-input' /><i ng-show='correct' class='icon ion-ios-checkmark input'></i>";
-    var hintInput = "<input type='text' ng-model='bindedModel' ng-change='gradeAnswer(bindedModel)' class='check-input'><i ng-hide='showHint || correct' class='icon ion-ios-help-empty input' ng-click='toggleHint()'></i><i ng-show='correct' class='icon ion-ios-checkmark input'></i><div ng-show='showHint' class='hint'>{{hint}}<i class='icon ion-ios-close' ng-click='toggleHint()'></i></div>"
+    var hintInput = "<input type='text' ng-model='bindedModel' ng-change='gradeAnswer(bindedModel)' class='check-input'><i ng-class='{show: !showHint && !correct}' class='icon ion-ios-help-empty input' ng-click='toggleHint()'></i><i ng-class='{show: correct}' class='icon ion-ios-checkmark input'></i><div ng-show='showHint' class='hint'>{{hint}}<i class='icon ion-ios-close' ng-click='toggleHint()'></i></div>"
 
     if (tAttrs.type == "textarea") {
       return alertTA;
