b2a5e6b8d5f34d954b60c4df02d4b43675cc08bb
David Carter <dajocarter@gmail.com>
2016-08-19 10:58:16 -0400

    Use bootstrap recommended browser compatibility

--- gulpfile.js
+++ gulpfile.js
@@ -4,15 +4,14 @@ var gulp = require('gulp'),
   browserSync = require('browser-sync').create();
 
 var AUTOPREFIXER_BROWSERS = [
-  'ie >= 10',
-  'ie_mob >= 10',
-  'ff >= 30',
-  'chrome >= 34',
-  'safari >= 7',
-  'opera >= 23',
-  'ios >= 7',
-  'android >= 4.4',
-  'bb >= 10'
+  "Android 2.3",
+  "Android >= 4",
+  "Chrome >= 20",
+  "Firefox >= 24",
+  "Explorer >= 8",
+  "iOS >= 6",
+  "Opera >= 12",
+  "Safari >= 6"
 ];
 
 gulp.task('images', function() {
