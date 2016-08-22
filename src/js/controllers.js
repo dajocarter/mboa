ea605b084a177df3c5984b3fc6c0442003b719a5
David Carter <dajocarter@gmail.com>
2016-08-22 01:28:35 -0400

    Add basic file upload capability

--- src/js/controllers.js
+++ src/js/controllers.js
@@ -42,3 +42,55 @@ angular.module('mboa').controller('ModalInstanceController', function ($scope, $
     $uibModalInstance.close();
   };
 });
+
+angular.module('mboa').controller('fileUploadController', function($scope, $stateParams) {
+  var auth = firebase.auth();
+  var databaseRef = firebase.database().ref();
+  var storageRef = firebase.storage().ref();
+  
+  function handleFileSelect(evt) {
+    evt.stopPropagation();
+    evt.preventDefault();
+    var file = evt.target.files[0];
+    var metadata = {
+      'contentType': file.type
+    };
+    storageRef.child('images/' + $stateParams.caseName + '/' + file.name).put(file, metadata).then(function(snapshot) {
+      console.log('Uploaded', snapshot.totalBytes, 'bytes.');
+      console.log(snapshot.metadata);
+      
+      var newImgKey = databaseRef.child('images/' + $stateParams.caseName).push({
+        url: snapshot.metadata.downloadURLs[0],
+        fullPath: snapshot.metadata.fullPath,
+        name: snapshot.metadata.name,
+        size: snapshot.metadata.size,
+        type: snapshot.metadata.type,
+      }).then(function(snapshot) {
+        console.log('saved image to database', snapshot);
+      }).catch(function(error) {
+        console.log('error saving img data to db', error);
+      });
+      
+      var url = snapshot.metadata.downloadURLs[0];
+      console.log('File available at', url);
+      document.getElementById('linkbox').innerHTML = '<a href="' +  url + '">Click For File</a>';
+    }).catch(function(error) {
+      console.error('Upload failed:', error);
+    });
+  }
+  
+  window.onload = function() {
+    document.getElementById('file').addEventListener('change', handleFileSelect, false);
+    document.getElementById('file').disabled = true;
+    auth.onAuthStateChanged(function(user) {
+      if (user) {
+        console.log('Anonymous user signed-in.', user);
+        document.getElementById('file').disabled = false;
+      } else {
+        console.log('There was no anonymous session. Creating a new anonymous user.');
+        // Sign the user in anonymously since accessing Storage requires the user to be authorized.
+        auth.signInAnonymously();
+      }
+    });
+  }
+});
