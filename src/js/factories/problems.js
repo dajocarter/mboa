09f589ccd76f5332007b9401d749e7ce0746867d
David Carter <dajocarter@gmail.com>
2016-08-23 22:24:29 -0400

    Add percent to options

--- src/js/factories/problems.js
+++ src/js/factories/problems.js
@@ -6,27 +6,32 @@ angular.module('mboa').factory('Problems', function() {
       "choice": "a",
       "text": "Reticulin fibers fill the stroma of the spleen",
       "correct": false,
-      "chosen": null
+      "chosen": null,
+      "percent": 0
     }, {
       "choice": "b",
       "text": "White pulp provides a location for B cells to proliferate and differentiate",
       "correct": true,
-      "chosen": null
+      "chosen": null,
+      "percent": 0
     }, {
       "choice": "c",
       "text": "In open circulation, penicillar arterioles dump blood directly into splenic cords",
       "correct": false,
-      "chosen": null
+      "chosen": null,
+      "percent": 0
     }, {
       "choice": "d",
       "text": "Stave cells present antigens to B lymphocytes in white pulp forming lymphoid nodules",
       "correct": false,
-      "chosen": null
+      "chosen": null,
+      "percent": 0
     }, {
       "choice": "e",
       "text": "T cells surround arterioles in the spleen, known as periarteriole lymphatic sheaths (PALS)",
       "correct": false,
-      "chosen": null
+      "chosen": null,
+      "percent": 0
     }]
   }, {
     "text": "During an infection, why do lymph nodes increase in size?",
@@ -35,27 +40,32 @@ angular.module('mboa').factory('Problems', function() {
       "choice": "a",
       "text": "B cell proliferation forming lymphoid nodules (follicles) in the medulla",
       "correct": false,
-      "chosen": null
+      "chosen": null,
+      "percent": 0
     }, {
       "choice": "b",
       "text": "Enlargement of the subcapsular sinus with plasma cells",
       "correct": false,
-      "chosen": null
+      "chosen": null,
+      "percent": 0
     }, {
       "choice": "c",
       "text": "Congestion of the medullary sinuses with antigen presenting cells (APCs)",
       "correct": true,
-      "chosen": null
+      "chosen": null,
+      "percent": 0
     }, {
       "choice": "d",
       "text": "Clonal proliferation of T cells in the cortex",
       "correct": false,
-      "chosen": null
+      "chosen": null,
+      "percent": 0
     }, {
       "choice": "e",
       "text": "Antigen presentation by follicular dendritic cells to B cells in the cortex",
       "correct": false,
-      "chosen": null
+      "chosen": null,
+      "percent": 0
     }]
   }, {
     "text": "An embryologic malformation in the 3rd pharyngeal (branchial) pouch results in decreased differentiation of thymic epithelial cells (TECs) leading to thymic hypoplasia. What pathology would you expect from this smaller thymus?",
@@ -64,27 +74,32 @@ angular.module('mboa').factory('Problems', function() {
       "choice": "a",
       "text": "Decreased antigen-presentation to developing thymocytes in the medulla (positive selection) and the x (negative selection)",
       "correct": false,
-      "chosen": null
+      "chosen": null,
+      "percent": 0
     }, {
       "choice": "b",
       "text": "Increased tight barriers between peripheral blood and the thymic stroma",
       "correct": false,
-      "chosen": null
+      "chosen": null,
+      "percent": 0
     }, {
       "choice": "c",
       "text": "Failure of thymic (Hassall's) corpuscles to form leading to decreased cytokine production for T cell development",
       "correct": true,
-      "chosen": null
+      "chosen": null,
+      "percent": 0
     }, {
       "choice": "d",
       "text": "An abundance of T-lymphocytes leading to overactive adaptive immune responses and autoimmunity",
       "correct": false,
-      "chosen": null
+      "chosen": null,
+      "percent": 0
     }, {
       "choice": "e",
       "text": "Both A and C are possibilities",
       "correct": false,
-      "chosen": null
+      "chosen": null,
+      "percent": 0
     }]
   }, {
     "text": "Which of the following is true regarding M cells?",
@@ -93,27 +108,32 @@ angular.module('mboa').factory('Problems', function() {
       "choice": "a",
       "text": "They function to increase efficiency of lymphoid nodule activity in the appendix",
       "correct": false,
-      "chosen": null
+      "chosen": null,
+      "percent": 0
     }, {
       "choice": "b",
       "text": "They concentrate antigens, dendritic cells, and lymphocytes in intracellular pockets",
       "correct": true,
-      "chosen": null
+      "chosen": null,
+      "percent": 0
     }, {
       "choice": "c",
       "text": "They produce IgA mediating inflammatory reactions for defense of the mucosal surface",
       "correct": false,
-      "chosen": null
+      "chosen": null,
+      "percent": 0
     }, {
       "text": "They produce hormones and other signaling molecules in response to antigen presentation in primary lymphoid organs",
       "choice": "d",
       "correct": false,
-      "chosen": null
+      "chosen": null,
+      "percent": 0
     }, {
       "choice": "e",
       "text": "They are synonymous with enterocytes",
       "correct": false,
-      "chosen": null
+      "chosen": null,
+      "percent": 0
     }]
   }, {
     "text": "Name the cell at the arrow tip.",
@@ -124,27 +144,32 @@ angular.module('mboa').factory('Problems', function() {
       "choice": "a",
       "text": "Lymphocyte",
       "correct": true,
-      "chosen": null
+      "chosen": null,
+      "percent": 0
     }, {
       "choice": "b",
       "text": "Monocyte",
       "correct": false,
-      "chosen": null
+      "chosen": null,
+      "percent": 0
     }, {
       "choice": "c",
       "text": "Basophilic erythroblast (N1)",
       "correct": false,
-      "chosen": null
+      "chosen": null,
+      "percent": 0
     }, {
       "choice": "d",
       "text": "Neutrophil",
       "correct": false,
-      "chosen": null
+      "chosen": null,
+      "percent": 0
     }, {
       "choice": "e",
       "text": "Metamyelocyte",
       "correct": false,
-      "chosen": null
+      "chosen": null,
+      "percent": 0
     }]
   }];
 });
